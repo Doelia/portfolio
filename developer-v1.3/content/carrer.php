c4691f0756eda47b77b47e8f24d2bcc4c58d4840
Doelia <kyxalia@gmail.com>
2015-03-14 20:48:24 +0100

    Mise en page page parcours

--- developer-v1.3/content/carrer.php
+++ developer-v1.3/content/carrer.php
@@ -1,4 +1,4 @@
-<section class="section">
+<section class="section carrer">
 	<div class="section-inner">
 		<p><a href="home.html">Retour à l'acceuil</a></p>
 		<br>
@@ -12,7 +12,7 @@
 				<div class="desc col-md-10 col-sm-8 col-xs-12">
 					<h3 class="titre">Premier contact - <small><i>15 ans</i></small></h3>
 					<p>
-						Jeune lycéen attiré par la programmation, je décide d’apprendre le langage C pour débuter. Je découvre les bases de l'algorithme mais je me limite à quelques programmes en console.
+						Jeune lycéen attiré par la programmation, je décide de débuter avec langage C. Je découvre les bases de l'algorithmie et je me limite à quelques programmes en console.
 					</p>
 				</div><!--//desc-->                          
 			</div><!--//item-->
@@ -25,55 +25,96 @@
 					<h3 class="titre">Découverte du monde du web - <small><i>16 ans</i></small></h3>
 					<p>
 						En manque de résultats concrets, je me tourne vers le monde du web.
-				Je met en ligne mon premier site web personnel en HTML/PHP.
-				Je pratique les langages du web activement pendant une année et je
-				développe une certaine démarche de développeur
+						Je met en ligne mon premier site web personnel en HTML/PHP.
+						Je pratique les langages du web activement pendant une année et
+						acquis une certaine démarche de développeur
 					</p>
 				</div><!--//desc-->                          
 			</div><!--//item-->
 
-			<h3 class="title">Première contribution - <small><i>2009, 17 ans</i></small></h3>
-			<p>
-				Je rejoint mon premier projet dans une équipe. Il s’agit d’un grand serveur privé pour un MMORPG.
-				Je suis au poste de webmaster et j’assure le développement technique du site web :
-				inscriptions, liasons au serveur de jeu, boutique virtuelle en ligne...
-			</p>
-			<p>
-				J'y apprend toute la rigueur nécessaire quand on développe pour un large public
-				et je découvre une passion pour le contact direct avec une communauté.
-			</p>
+			<div class="item row">
+				<div class="col-md-2 col-sm-4 col-xs-12" href="" target="_blank">
+					<div class="rond">2009</div>
+				</div>
+				<div class="desc col-md-10 col-sm-8 col-xs-12">
+					<h3 class="titre">Première contribution - <small><i>17 ans</i></small></h3>
+					<p>
+						Je rejoint mon premier projet dans une équipe. Il s’agit d’un grand serveur privé pour un MMORPG.
+						Je suis au poste de webmaster et j’assure le développement technique du site web :
+						inscriptions, liasons au serveur de jeu, boutique virtuelle en ligne...
+					</p>
+					<p>
+						J'y apprend toute la rigueur nécessaire quand on développe pour un large public
+						et je découvre une passion pour le contact direct avec une communauté.
+					</p>
+				</div><!--//desc-->                          
+			</div><!--//item-->
 
-			<h3 class="title">Tentative de projet personnel - <small><i>2010, 18 ans</i></small></h3>
-			<p>
-				Je décide de me lancer dans mon propre projet 100% web, en développant un jeu de gestion de base par navigateur :
-				Kystelia. J’en arrive à la fin d’une bêta fonctionnelle, mais pas assez intéressante
-				pour être mis en production. J’abandonne le projet mais j’y ai appris beaucoup en conception
-				et architecture (Organisation du code, utilisation de la POO, communication AJAX…).
-			</p>
-			<p>
-				J’entre dans une école d’informatique pendant cette année et découvre une certaine aisance dans tout le
-				domaines de l’informatique, autant en programmation qu’en conception.
-			</p>
+			<div class="item row">
+				<div class="col-md-2 col-sm-4 col-xs-12" href="" target="_blank">
+					<div class="rond">2010</div>
+				</div>
+				<div class="desc col-md-10 col-sm-8 col-xs-12">
+					<h3 class="titre">Tentative de projet personnel - <small><i>18 ans</i></small></h3>
+					<p>
+						Je décide de me lancer dans mon propre projet 100% web, en développant un jeu de gestion de base par navigateur :
+						Kystelia. J’en arrive à la fin d’une bêta fonctionnelle, mais pas assez intéressante
+						pour être mis en production. J’abandonne le projet mais j’y apprend beaucoup en conception
+						et architecture (Organisation du code, utilisation de la POO, communication AJAX…).
+					</p>
+					<p>
+						J’entre dans une école d’informatique pendant cette année avec une certaine aisance dans tout le
+						domaines de l’informatique, autant en programmation qu’en conception.
+					</p>
+				</div><!--//desc-->                          
+			</div><!--//item-->
 
-			<h3 class="title">Première gestion - <small><i>2011, 19 ans</i></small></h3>
-			<p>
-				Je me tourne ensuite vers les applications et plus particulièrement au Java coté serveur.
-			</p>
-			<p>
-				Je fonde à mon tour un nouveau projet de serveur privé pour un MMORPG, en tant que chef de projet et développeur principal.
-				J’y développe toutes les capacités nécessaire en gestion de serveur : Hébergement, administration, UNIX,
-				optimisation, gestion des charges… Je me découvre une satisfacation pour le contact avec une jeune communauté.
-				Ce fut mon premier projet personnel avec une réussite. Les serveurs sont montés à des records de 200 connectés simultanés.
-			</p>
-			<p>Je réalise cette année là un stage dans une agence web qui m’apprend le travail en équipe et la rigueur professionnelle.</p>
+			<div class="item row">
+				<div class="col-md-2 col-sm-4 col-xs-12" href="" target="_blank">
+					<div class="rond">2011</div>
+				</div>
+				<div class="desc col-md-10 col-sm-8 col-xs-12">
+					<h3 class="titre">Première gestion - <small><i>19 ans</i></small></h3>
+					<p>
+						Je me tourne ensuite vers les applications et plus particulièrement au développement coté serveur avec Java.
+					</p>
+					<p>
+						Je fonde à mon tour un nouveau projet de serveur privé pour un MMORPG, en tant que chef de projet et développeur principal.
+						J’y développe toutes les capacités nécessaire pour gestion de serveurs : Hébergement, administration, UNIX,
+						optimisation, gestion des charges...
+						Ce fut mon premier projet personnel avec une réussite. Les serveurs sont montés à des records de 200 connectés simultanés.
+					</p>
+					<p>Je réalise cette année là un stage dans une agence web qui m’apprend le travail en équipe et la rigueur professionnelle.</p>
+				</div><!--//desc-->                          
+			</div><!--//item-->
 
-			<h3 class="title">Fondation de SURVIVIA Multgaming - <small><i>2012, 20 ans</i></small></h3>
-			<p>Je découvre l’univers de Minecraft et surtout sa liberté. Je me lance dans de nouveaux serveurs de jeux en tant que gérant et je fonde la société <a href="http://survivia.net" target="_blank">SURVIVIA</a>, pour du durable cette fois. Le projet tourne depuis 2 ans et demi et est correctement maintenu avec plusieurs milliers de joueurs chaque jours.</p>
-			<p>J’ai eu l’opportunité d’organiser le premier tournoi Minecraft Hunger Games de France en LAN grace à l’association Virtual Lan.</p>
+			<div class="item row">
+				<div class="col-md-2 col-sm-4 col-xs-12" href="" target="_blank">
+					<div class="rond">2012</div>
+				</div>
+				<div class="desc col-md-10 col-sm-8 col-xs-12">
+					<h3 class="titre">Fondation de SURVIVIA Multgaming - <small><i>20 ans</i></small></h3>
+					<p>Je découvre l’univers de Minecraft et surtout sa liberté. Je me lance dans de nouveaux serveurs de jeux en tant que gérant et je fonde la société <a href="http://survivia.net" target="_blank">SURVIVIA</a>, pour du durable cette fois. Le projet tourne depuis 2 ans et demi et est correctement maintenu avec plusieurs milliers de joueurs chaque jours.</p>
+					<p>J’ai eu l’opportunité d’organiser le premier tournoi Minecraft Hunger Games de France en LAN grace à l’association <i>Virtual Lan</i>.</p>
+				</div><!--//desc-->                          
+			</div><!--//item-->
 
-			<h3 class="title">Continuité</h3>
-			<p>Tout en continuant à maintenir SURVIVIA, je continue à développer de nouvelles capacités dans tout les langages et domaines. Je suis entré en 2014 en Master Informatique - spécialité Imagerie, Intelligence artificielle et jeux vidéo. J'y découvre les ficelles des moteurs de jeux coté client. Je travaille actuellement sur un concept de jeu de rythme pour mobile avec le moteur de jeu Unity.</p>
-			<p>En simultané de ces projets principaux, j’ai aussi aussi produit quelques réalisations annexes. Elles sont toutes consultables sur cette page. (Démo / Screenshots / Code sources…)</p>
+			<div class="item row">
+				<div class="col-md-2 col-sm-4 col-xs-12" href="" target="_blank">
+					<div class="rond">2015</div>
+				</div>
+				<div class="desc col-md-10 col-sm-8 col-xs-12">
+					<h3 class="titre">Continuité - <small><i>22 ans</i></small></h3>
+					<p>Tout en continuant à maintenir SURVIVIA, je continue à développer de nouvelles capacités dans
+					tout les langages et domaines. Je suis entré en 2014 en Master Informatique - 
+					<i>spécialité Imagerie, Intelligence artificielle et jeux vidéo</i>.
+					J'y découvre les ficelles des moteurs de jeux coté client. Je travaille actuellement sur un concept
+					de jeu de rythme pour mobile avec le moteur de jeu Unity.</p>
+					<p>En simultané de ces projets principaux, j’ai aussi aussi produit quelques réalisations annexes. 
+					lles sont toutes consultables sur cette page. (Démo / Screenshots / Code sources…)</p>
+				</div><!--//desc-->                          
+			</div><!--//item-->
+
 		</div><!--//content-->  
 	</div><!--//section-inner-->                 
 </section><!--//section-->
